c46d68913813c669508f0eace85a7897b35bb30c
Jorris-N <nyangejorris@gmail.com>
2024-11-20 15:24:01 +0300

    new commit

---
+++ app/utils/defaultStates.tsx
@@ -0,0 +1,39 @@
+const defaultStates = [
+  {
+    name: "Nairobi",
+    country: "KE",
+    state: "Nairobi",
+    lat: -1.286389,
+    lon: 36.817223,
+  },
+  {
+    name: "Dar es Salaam",
+    country: "TZ",
+    state: "Dar es Salaam",
+    lat: -6.792354,
+    lon: 39.208328,
+  },
+  {
+    name: "Mombasa",
+    country: "KE",
+    state: "Mombasa",
+    lat: -4.043477,
+    lon: 39.668207,
+  },
+  {
+    name: "Malindi",
+    country: "KE",
+    state: "Kilifi",
+    lat: -3.219186,
+    lon: 40.116898,
+  },
+  {
+    name: "Eldoret",
+    country: "KE",
+    state: "Uasin Gishu",
+    lat: 0.514277,
+    lon: 35.269780,
+  },
+];
+
+export default defaultStates;
